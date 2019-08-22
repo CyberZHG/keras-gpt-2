acb7e8a4139a8a39e4ba9e5263231009562a5ae5
CyberZHG <853842+CyberZHG@users.noreply.github.com>
2019-08-23 07:25:21 +0800

    Fix compatibility issues with Keras 2.2.5

--- setup.py
+++ setup.py
@@ -12,19 +12,18 @@ with codecs.open('requirements.txt', 'r', 'utf8') as reader:
 
 setup(
     name='keras-gpt-2',
-    version='0.12.0',
+    version='0.13.0',
     packages=find_packages(),
     url='https://github.com/CyberZHG/keras-gpt-2',
     license='MIT',
     author='CyberZHG',
-    author_email='CyberZHG@gmail.com',
+    author_email='CyberZHG@users.noreply.github.com',
     description='GPT-2',
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=install_requires,
     classifiers=(
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
